74d928a477fb73ab94f5184c2d44a6b169f44264
Anil Kumar <anillakhlan2107@gmail.com>
2025-01-18 00:28:34 +0530

    first example of querying data

--- schema.js
+++ schema.js
@@ -3,8 +3,23 @@ const graphql = require('graphql');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
+const _ = require('lodash');
+
+const users = [
+    {
+        id: "1",
+        firstName: 'Anil',
+        age: 25
+    },
+    {
+        id: "2",
+        firstName: 'Kavita',
+        age: 27
+    }
+]
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -19,4 +34,21 @@ const UserType = new GraphQLObjectType({
             type: GraphQLInt
         },
     }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString}},
+            resolve (parentValue, args) {
+                return _.find(users, {id: args.id});
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
 })
