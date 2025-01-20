937ab9edbbdcddf3bbcbb91c1165c10ad767abff
Anil Kumar <anillakhlan2107@gmail.com>
2025-01-20 16:32:27 +0530

    add react router

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import SongList  from './components/songList';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import App from './components/app'
 const client = new ApolloClient({});
+
 
 const Root = () => {
   return <ApolloProvider client={client}>
-      <SongList />
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
   </ApolloProvider>
 };
 
