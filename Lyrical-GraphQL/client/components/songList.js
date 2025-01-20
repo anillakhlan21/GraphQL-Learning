1fe1c5de3ced415a194b04093c39625347831d53
Anil Kumar <anillakhlan2107@gmail.com>
2025-01-20 14:50:59 +0530

    bonding queries with componenets

--- Lyrical-GraphQL/client/components/songList.js
+++ Lyrical-GraphQL/client/components/songList.js
@@ -1,17 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import gql from 'graphql-tag'
+import { graphql } from 'react-apollo';
 
-class SongList extends Component{
-    render(){
+class SongList extends Component {
+    render() {
+        console.log(this.props)
+
         return <div>
             Song List
         </div>
     }
 }
 
-const query = query`
-
+const query = gql`
+  query GetSongs {
+    songs {
+      title
+    }
+  }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
 
