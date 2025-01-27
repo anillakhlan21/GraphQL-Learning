ab33f09e2f21f455df66052cd16a393b11451bbf
Anil Kumar <anillakhlan2107@gmail.com>
2025-01-27 23:00:24 +0530

    optimistic ui updated

--- Lyrical-GraphQL/client/components/lyricList.js
+++ Lyrical-GraphQL/client/components/lyricList.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import likeLyricMutation from '../queries/likeLyric';
 import mutation from '../queries/likeLyric';
+import { optimistic } from 'apollo-client/optimistic-data/store';
 class LyricList extends Component {
     constructor(props) {
         super(props);
     }
 
-    onLikeButtonClick(lyricId) {
-        this.props.mutate({ variables: { id: lyricId } })
+    onLikeButtonClick(lyricId, likes) {
+        this.props.mutate({ variables: { id: lyricId }, optimisticResponse: {
+            __typename: "Mutation",
+            likeLyric: {
+                __typename: "LyricType",
+                id: lyricId,
+                likes: likes+1
+            },
+
+        } })
     }
 
     render() {
@@ -21,7 +30,7 @@ class LyricList extends Component {
                             return <li className="collection-item" key={lyric.id}>
                                 {lyric.content}
                                 <div className="vote-box">
-                                        <i className="material-icons" onClick={() => { this.onLikeButtonClick(lyric.id) }}>
+                                        <i className="material-icons" onClick={() => { this.onLikeButtonClick(lyric.id, lyric.likes) }}>
                                             thumb_up
                                         </i>
                                         {lyric.likes || 0}
