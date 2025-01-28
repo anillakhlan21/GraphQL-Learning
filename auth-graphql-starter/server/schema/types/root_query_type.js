5a3287e28139aef72b73bb8e48177c4927e8683c
Anil Kumar <anillakhlan2107@gmail.com>
2025-01-28 22:26:46 +0530

    defining user type and mongo uri

--- auth-graphql-starter/server/schema/types/root_query_type.js
+++ auth-graphql-starter/server/schema/types/root_query_type.js
@@ -1,8 +1,14 @@
 const graphql = require('graphql');
+const UserType = require('./user_type');
 const { GraphQLObjectType } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType'
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType
+    }
+  }
 });
 
 module.exports = RootQueryType;
