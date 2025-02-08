f123971ec4c9b85313ed8e5b580e0f3b158ff620
Anil Kumar <anillakhlan2107@gmail.com>
2025-02-08 09:09:53 +0530

    authForm: setState instead of direct assignment and css changes

--- auth-graphql-starter/client/components/authForm.js
+++ auth-graphql-starter/client/components/authForm.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 
 class AuthForm extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '' }
+    }
+
     renderConfirmPasswordField() {
         return (
-            <div>
+            <div className="input-field">
                 <label>Confirm Password</label>
                 <input onChange={(event) => this.setState({ confirmPassword: event.target.value })} />
             </div>
@@ -13,14 +18,18 @@ class AuthForm extends Component {
 
     render() {
         return (
-            <div>
-                <form>
-                    <label>Email</label>
-                    <input onChange={(event) => this.state.email = event.target.value} />
-                    <label>Password</label>
-                    <input onChange={(event) => this.state.password = event.target.value} />
+            <div className="row">
+                <form className="col s4">
+                    <div className="input-field">
+                        <label>Email</label>
+                        <input onChange={(event) => this.setState({ email: this.target.value })} />
+                    </div>
+                    <div className="input-field">
+                        <label>Password</label>
+                        <input onChange={(event) => this.setState({ password: event.target.value })} />
+                    </div>
                     {this.props.isSignUpForm && this.renderConfirmPasswordField()}
-                    <button onClick={() => this.props.onSubmit()}>{this.props.buttonName}</button>
+                    <button className="btn" onClick={() => this.props.onSubmit()}>{this.props.buttonName}</button>
                 </form>
             </div>
         )
